8a99c35c7f5e79385c2a962531c73352db87d402
aarushtechnologies <aarushtechnologies7@gmail.com>
2026-01-20 15:10:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Codingwale vaijapur - hello')
+  res.send('Codingwale vaijapur')
 })
 
 app.listen(port,'0.0.0.0', () => {
