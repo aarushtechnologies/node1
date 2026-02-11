5e0320dc343815798011932af2ad8241d8614a91
aarushtechnologies <aarushtechnologies7@gmail.com>
2026-02-12 03:08:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>1.0.0 Lived - Node js application</h1>')
+  res.send('<h1>2.0.0 Lived - Node js application <br> using nexus </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
